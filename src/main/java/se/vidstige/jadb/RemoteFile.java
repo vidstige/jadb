8636195fdfb47b38b9fa6d135bc633957b631bfd
andoni <andoni.delolmo@gmail.com>
2016-02-28 13:26:53 +0100

    remove proprietary exception.

--- src/main/java/se/vidstige/jadb/RemoteFile.java
+++ src/main/java/se/vidstige/jadb/RemoteFile.java
@@ -1,21 +1,34 @@
 package se.vidstige.jadb;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * Created by vidstige on 2014-03-20
  */
 public class RemoteFile {
     private final String path;
 
-    public RemoteFile(String path) { this.path = path;}
+    public RemoteFile(String path) {
+        this.path = path;
+    }
 
-    public String getName() { throw new NotImplementedException(); }
-    public int getSize() { throw new NotImplementedException(); }
-    public long getLastModified() { throw new NotImplementedException(); }
-    public boolean isDirectory() { throw new NotImplementedException(); }
+    public String getName() {
+        throw new IllegalStateException("Not implemented");
+    }
 
-    public String getPath() { return path;}
+    public int getSize() {
+        throw new IllegalStateException("Not implemented");
+    }
+
+    public long getLastModified() {
+        throw new IllegalStateException("Not implemented");
+    }
+
+    public boolean isDirectory() {
+        throw new IllegalStateException("Not implemented");
+    }
+
+    public String getPath() {
+        return path;
+    }
 
     @Override
     public boolean equals(Object o) {
