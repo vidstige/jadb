00cc52d889b7a3040948e8f6c8ad6216f57996d0
Arthur <art.enikoff@gmail.com>
2016-07-20 17:11:58 +0300

    #IMP-2-install-options skeleton created

--- src/se/vidstige/jadb/managers/PackageManager.java
+++ src/se/vidstige/jadb/managers/PackageManager.java
@@ -10,6 +10,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Java interface to package manager. Launches package manager through jadb
@@ -52,10 +53,10 @@ public class PackageManager {
         Stream.readAll(s, Charset.forName("UTF-8"));
     }
 
-    public void install(File apkFile, List<String> extraArguments) throws IOException, JadbException {
+    private void install(File apkFile, List<String> extraArguments) throws IOException, JadbException {
         RemoteFile remote = new RemoteFile("/sdcard/tmp/" + apkFile.getName());
         device.push(apkFile, remote);
-        ArrayList<String> arguments = new ArrayList<String>();
+        ArrayList<String> arguments = new ArrayList<>();
         arguments.add("install");
         arguments.addAll(extraArguments);
         arguments.add(remote.getPath());
@@ -66,11 +67,16 @@ public class PackageManager {
     }
 
     public void install(File apkFile) throws IOException, JadbException {
-        install(apkFile, new ArrayList<String>(0));
+        install(apkFile, new ArrayList<>(0));
+    }
+
+    public void installWithOptions(File apkFile, List<? extends InstallOptions> options) throws IOException, JadbException {
+        List<String> optionsStr = options.stream().map(InstallOptions::getStringRepresentation).collect(Collectors.toList());
+        install(apkFile, optionsStr);
     }
 
     public void forceInstall(File apkFile) throws IOException, JadbException {
-        install(apkFile, Collections.singletonList("-r"));
+        installWithOptions(apkFile, Collections.singletonList(new REINSTALL_KEEPING_DATA()));
     }
 
     public void uninstall(Package name) throws IOException, JadbException {
@@ -82,4 +88,75 @@ public class PackageManager {
     public void launch(Package name) throws IOException, JadbException {
         InputStream s = device.executeShell("monkey", "-p", name.toString(), "-c", "android.intent.category.LAUNCHER", "1");
     }
+
+    //<editor-fold desc="InstallOptions">
+    public static abstract class InstallOptions {
+        InstallOptions(String ... varargs) {
+            for(String str: varargs) {
+                stringBuilder.append(str).append(" ");
+            }
+        }
+
+        private final StringBuilder stringBuilder = new StringBuilder();
+
+        private String getStringRepresentation() {
+            return stringBuilder.toString();
+        }
+    }
+
+
+    public static class WITH_FORWARD_LOCK extends InstallOptions {
+        public WITH_FORWARD_LOCK() {
+            super("-l");
+        }
+    }
+
+    public static class REINSTALL_KEEPING_DATA extends InstallOptions {
+        public REINSTALL_KEEPING_DATA() {
+            super("-r");
+        }
+    }
+
+    public static final class ALLOW_TEST_APK extends InstallOptions {
+        public ALLOW_TEST_APK() {
+            super("-t");
+        }
+    }
+
+    public static final class WITH_INSTALLER_PACKAGE_NAME extends InstallOptions {
+        public WITH_INSTALLER_PACKAGE_NAME(String name) {
+            super("-t", name);
+        }
+    }
+
+    public static final class ON_SHARED_MASS_STORAGE extends InstallOptions {
+        public ON_SHARED_MASS_STORAGE(String name) {
+            super("-s", name);
+        }
+    }
+
+    public static final class ON_INTERNAL_SYSTEM_MEMORY extends InstallOptions {
+        public ON_INTERNAL_SYSTEM_MEMORY(String name) {
+            super("-f", name);
+        }
+    }
+
+    public static final class ALLOW_VERSION_DOWNGRADE extends InstallOptions {
+        public ALLOW_VERSION_DOWNGRADE() {
+            super("-d");
+        }
+    }
+
+    public static final class GRANT_ALL_PERMISSIONS extends InstallOptions {
+        public GRANT_ALL_PERMISSIONS() {
+            super("-g");
+        }
+    }
+
+    public static final class CUSTOM_PARAMETER extends InstallOptions {
+        public CUSTOM_PARAMETER(String ... varargs) {
+            super(varargs);
+        }
+    }
+    //</editor-fold>
 }
