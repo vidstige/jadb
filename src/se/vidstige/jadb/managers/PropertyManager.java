7a245bde01e7beddfa9215d4f6b9f8b61f2d4d66
Pablo Baxter <pablo@pathsense.com>
2017-02-02 16:19:37 -0800

    Reverting PropertyManager classes

--- src/se/vidstige/jadb/managers/PropertyManager.java
+++ src/se/vidstige/jadb/managers/PropertyManager.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,45 +14,16 @@ import java.util.regex.Pattern;
 /**
  * A class which works with properties, uses getprop and setprop methods of android shell
  */
-public class PropertyManager implements Iterable<PropertyManager.Entry>{
+public class PropertyManager {
     private final JadbDevice device;
-    private Map<String, String> map;
 
-    public PropertyManager(JadbDevice device) throws IOException, JadbException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(device.executeShell("getprop")));
+    public PropertyManager(JadbDevice device) {
         this.device = device;
-        this.map = parseProp(reader);
-        reader.close();
     }
 
-    public int size(){
-        return map.size();
-    }
-
-    public boolean containsKey(String key){
-        return map.containsKey(key);
-    }
-
-    public String get(String key){
-        return map.get(key);
-    }
-
-    public String set(String key, String val) throws JadbException, IOException {
-        setProperty(key, val);
-        return map.put(key, val);
-    }
-
-    @Override
-    public Iterator<Entry> iterator() {
-        return new PropertyItr();
-    }
-
-    private void setProperty(String key, String val) throws JadbException, IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(device.executeShell("setprop", key, val)));
-        String line;
-        while((line = reader.readLine()) != null){
-            System.out.println(line);
-        }
+    public Map<String, String> getprop() throws IOException, JadbException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(device.executeShell("getprop")));
+        return parseProp(bufferedReader);
     }
 
     private Map<String, String> parseProp(BufferedReader bufferedReader) throws IOException {
@@ -69,7 +39,7 @@ public class PropertyManager implements Iterable<PropertyManager.Entry>{
 
             if (matcher.find()) {
                 if (matcher.groupCount() < 2) {
-                    System.err.println("Property line: " + line + " does not match pattern. Ignoring");
+                    System.err.println("Property line: " + line + " does not match patter. Ignoring");
                     continue;
                 }
                 String key = matcher.group(1);
@@ -79,50 +49,5 @@ public class PropertyManager implements Iterable<PropertyManager.Entry>{
         }
 
         return result;
-    }
-
-    class Entry{
-        private Map.Entry<String, String> entry;
-
-        private Entry(Map.Entry<String, String> entry){
-            this.entry = entry;
-        }
-
-        public String getKey(){
-            return entry.getKey();
-        }
-
-        public String getValue(){
-            return entry.getValue();
-        }
-
-        public void setValue(String val) throws IOException, JadbException {
-            setProperty(entry.getKey(), val);
-            entry.setValue(val);
-        }
-    }
-
-    private class PropertyItr implements Iterator<Entry> {
-
-        Iterator<Map.Entry<String, String>> it;
-
-        private PropertyItr(){
-            it = map.entrySet().iterator();
-        }
-
-        @Override
-        public boolean hasNext() {
-            return it.hasNext();
-        }
-
-        @Override
-        public Entry next() {
-            return new Entry(it.next());
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException("remove");
-        }
     }
 }
